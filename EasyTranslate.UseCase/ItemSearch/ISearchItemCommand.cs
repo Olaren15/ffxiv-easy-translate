c16a7a7b962f60b1dd2d4840f762630a95e0a09c
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-02-02 23:44:27 -0500

    Scaffold project architecture

---
+++ EasyTranslate.UseCase/ItemSearch/ISearchItemCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyTranslate.UseCase.ItemSearch;
+
+using EasyTranslate.Domain.Entities;
+
+public interface ISearchItemCommand
+{
+    public IEnumerable<Item> SearchItem(ItemSearchQuery query);
+}
