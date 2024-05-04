24ca1d0e216d4040bb9d3789f6421532591b5586
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-05-04 19:51:47 -0400

    Added Orchestrion adapter

---
+++ EasyTranslate.Infrastructure/GameData/Adapters/OrchestrionAdapter.cs
@@ -0,0 +1,33 @@
+﻿namespace EasyTranslate.Infrastructure.GameData.Adapters;
+
+using Domain.Entities;
+using Lumina.Excel;
+using Lumina.Excel.GeneratedSheets2;
+using ContentType = Domain.Entities.ContentType;
+
+public class OrchestrionAdapter : IContentTypeAdapter<Orchestrion>
+{
+    private const uint MusicNoteIcon = 76406;
+
+    public Func<Orchestrion, bool> WhereClause(string searchName)
+    {
+        return orchestrion => orchestrion.Name.RawString.Contains(searchName, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public Func<Orchestrion, Content> MapToContent(
+        ExcelSheet<Orchestrion> englishSheet,
+        ExcelSheet<Orchestrion> frenchSheet,
+        ExcelSheet<Orchestrion> germanSheet,
+        ExcelSheet<Orchestrion> japaneseSheet
+    )
+    {
+        return orchestrion => new Content(
+            ContentType.Orchestrion,
+            MusicNoteIcon,
+            englishSheet.GetRow(orchestrion.RowId)!.Name.RawString,
+            frenchSheet.GetRow(orchestrion.RowId)!.Name.RawString,
+            germanSheet.GetRow(orchestrion.RowId)!.Name.RawString,
+            japaneseSheet.GetRow(orchestrion.RowId)!.Name.RawString
+        );
+    }
+}
--- EasyTranslate.Infrastructure/GameData/GameDataContentRepository.cs
+++ EasyTranslate.Infrastructure/GameData/GameDataContentRepository.cs
@@ -23,7 +23,7 @@ public class GameDataContentRepository(IEnumerable<ISearchByNameQuery> searhQuer
         /*
          TODO: Achieve feature-parity with the previous XivApiContentRepository.
          Missing fields:
-          BuddyEquip, Orchestrion, PlaceName, Weather
+          PlaceName, Weather
          */
 
         // TODO: Sort results by relevancy
