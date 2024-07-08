3acb19d57236df9071018f8fe4576940db280f34
Catherine Gilbert <catherine.gilbert15@gmail.com>
2024-07-07 20:11:36 -0400

    Allow searching for classes and jobs (#39)
    * Allow searching for classes and jobs
    
    * Added integration test

---
+++ EasyTranslate.Infrastructure/GameData/Adapters/ClassJobAdapter.cs
@@ -0,0 +1,28 @@
+﻿using EasyTranslate.Domain.Entities;
+using EasyTranslate.Infrastructure.GameData.Sheets;
+
+namespace EasyTranslate.Infrastructure.GameData.Adapters;
+
+public class ClassJobAdapter : IContentTypeAdapter<ClassJobLite>
+{
+    private const uint BaseJobIconId = 62100;
+
+    public Func<ClassJobLite, bool> WhereClause(string searchName)
+    {
+        return classJob => classJob.RowId != 0 && // Filter out Adventurer
+                           classJob.Name.RawString.Contains(searchName, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public Func<ClassJobLite, Content> MapToContent(ClassJobLite english, ClassJobLite french, ClassJobLite german,
+        ClassJobLite japanese)
+    {
+        return classJob => new Content(
+            ContentType.ClassJob,
+            BaseJobIconId + classJob.RowId,
+            english.Name.RawString,
+            french.Name.RawString,
+            german.Name.RawString,
+            japanese.Name.RawString
+        );
+    }
+}
